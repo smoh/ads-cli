cf70be94deca328f0945ee1d3bd246b3f1e386a2
Semyeong Oh <semyeong.oh@gmail.com>
2019-06-19 17:11:51 +0100

    Trying many new stuff

--- adsapp.py
+++ adsapp.py
@@ -1,5 +1,5 @@
 from prompt_toolkit import Application, HTML
-from prompt_toolkit.buffer import Buffer
+from prompt_toolkit.buffer import Buffer, reshape_text
 from prompt_toolkit.layout.containers import VSplit, HSplit, Window
 from prompt_toolkit.layout.controls import BufferControl, FormattedTextControl
 from prompt_toolkit.layout.layout import Layout
@@ -7,6 +7,8 @@ from prompt_toolkit.key_binding import KeyBindings
 from prompt_toolkit.application import get_app
 from prompt_toolkit.styles import Style
 from prompt_toolkit.key_binding.bindings.focus import focus_next, focus_previous
+from prompt_toolkit.shortcuts import message_dialog
+from prompt_toolkit import PromptSession
 from prompt_toolkit.widgets import (
     Box,
     Button,
@@ -20,19 +22,28 @@ from prompt_toolkit.widgets import (
     RadioList,
     TextArea,
 )
-
-
+from prompt_toolkit.completion import WordCompleter
+import webbrowser
 import pickle
+import ads
 
+ads_query_completer = WordCompleter(
+    ["author", "first_author", "year", "property", "title"], ignore_case=True
+)
+
+# dummy content to format stuff
 with open("dump.pkl", "rb") as f:
     q = pickle.load(f)
 
 kb = KeyBindings()
+kb_search = KeyBindings()
+kb_output = KeyBindings()
 
-buffer1 = Buffer()  # Editable buffer.
+kb_output.add("j")(focus_next)
+kb_output.add("k")(focus_previous)
 
 
-@kb.add("q")
+@kb.add("c-d")
 def exit_(event):
     """
     Pressing Ctrl-Q will exit the user interface.
@@ -40,85 +51,168 @@ def exit_(event):
     Setting a return value means: quit the event loop that drives the user
     interface and return this value from the `Application.run()` call.
     """
-    event.app.exit()
+    event.app.exit(result=[result, duh])
 
 
-@kb.add("a")
-def exit_(event):
+@kb_output.add("n")
+def next_(event):
     """
     Pressing Ctrl-Q will exit the user interface.
 
     Setting a return value means: quit the event loop that drives the user
     interface and return this value from the `Application.run()` call.
     """
-    get_app().layout.focus(items[0])
+    app.page += 1
+    n = app.page
+    items = []
+    for i in range(10 * n, 10 * (n + 1)):
+        w = Window(
+            height=2,
+            content=FormattedTextControl(
+                focusable=True,
+                text=HTML(
+                    f"{i:2d} {q.articles[i].title[0][:]}\n"
+                    f"   <u>{q.articles[i].first_author}</u>"
+                    f"{q.articles[i].year} {q.articles[i].bibcode}"
+                ),
+            ),
+        )
+        w.bibcode = q.articles[i].bibcode
+        items.append(w)
+    event.app.layout.container.children = [
+        Window(height=3, content=BufferControl(buffer=buffer1)),
+        *items,
+    ]
+    event.app.layout.focus(items[0])
 
 
-kb_elem = KeyBindings()
-kb.add("j")(focus_next)
-kb.add("k")(focus_previous)
+@kb_search.add("c-j")
+def go_to_output(event):
+    event.app.layout.focus(event.app.layout.container.children[1])
 
 
-selected_style = Style.from_dict({"window.border": "#238400"})
+@kb_search.add("c-s")
+def search_(event):
+    q = buffer1.text
+    duh.append(q)
+    q = ads.SearchQuery(q=q)
+    list(q)
+
+    items = []
+    for i in range(min([10, len(q.articles)])):
+        w = Window(
+            height=2,
+            content=FormattedTextControl(
+                focusable=True,
+                text=HTML(
+                    f"{i:2d} {q.articles[i].title[0][:]}\n"
+                    f"   <u>{q.articles[i].first_author}</u>"
+                    f"{q.articles[i].year} {q.articles[i].bibcode}"
+                ),
+            ),
+        )
+        w.bibcode = q.articles[i].bibcode
+        items.append(w)
+    output.children = items
+    event.app.layout.focus(output)
 
 
-@kb_elem.add("c")
+@kb_output.add("c")
 def select_(event):
     # event.app.layout.current_window.content = FormattedTextControl(text="dksjfksdjf")
     # event.app.layout.current_window.style = "bg:red"
-    event.app.layout.current_window.style = "bg:blue"
+    cw = event.app.layout.current_window
+    # event.app.get_parent(cw).style = "bg:blue"
+    if cw.bibcode in result:
+        cw.style = "bg:"
+        cw.content.style = "bg:"
+        result.discard(cw.bibcode)
+    else:
+        cw.style = "bg:blue"
+        cw.content.style = "bg:blue"
+        result.add(cw.bibcode)
 
 
-items = [
-    Window(
+@kb_output.add("o")
+def select_(event):
+    # event.app.layout.current_window.content = FormattedTextControl(text="dksjfksdjf")
+    # event.app.layout.current_window.style = "bg:red"
+    cw = event.app.layout.current_window
+    url = f"https://ui.adsabs.harvard.edu/abs/{cw.bibcode}"
+    webbrowser.open_new_tab(url)
+
+
+@kb_output.add("p")
+def select_(event):
+    # event.app.layout.current_window.content = FormattedTextControl(text="dksjfksdjf")
+    # event.app.layout.current_window.style = "bg:red"
+    cw = event.app.layout.current_window
+    url = f"https://ui.adsabs.harvard.edu/link_gateway/{cw.bibcode}/PUB_PDF"
+    webbrowser.open_new_tab(url)
+
+
+@kb_output.add("a")
+def select_(event):
+    cw = event.app.layout.current_window
+    bibcode = cw.bibcode
+    q = ads.SearchQuery(q=f"bibcode:{bibcode}", fl="abstract")
+    abstract = q.next().abstract
+    # duh.append(abstract)
+    # event.app.layout.container.children[0].content.content = "fudksdjfdksj"
+    statusbar.body.content.text = abstract
+    # reshape_text(buffer1)
+    # message_dialog(title="sdlkfj", text="lorem")
+
+
+buffer1 = Buffer(
+    completer=ads_query_completer, complete_while_typing=True
+)  # Editable buffer.
+searchbar = Frame(
+    title="Search",
+    body=HSplit(
+        [Window(height=3, content=BufferControl(buffer=buffer1), wrap_lines=True)]
+    ),
+    key_bindings=kb_search,
+)
+
+output = HSplit([], key_bindings=kb_output)
+
+statusbar = Frame(
+    Window(wrap_lines=True, height=0, content=FormattedTextControl(text="abcde"))
+)
+
+result = set()
+duh = []
+# page = 0
+
+
+items = []
+for i in range(10):
+    w = Window(
         height=2,
         content=FormattedTextControl(
             focusable=True,
             text=HTML(
-                f"{i:2d} {q.articles[i].title[0][:85]}\n"
-                f"   <u>{q.articles[i].first_author}</u> {q.articles[i].year} {q.articles[i].bibcode}"
+                f"{i:2d} {q.articles[i].title[0]}\n"
+                f"   <u>{q.articles[i].first_author}</u>"
+                f"{q.articles[i].year} {q.articles[i].bibcode}"
             ),
-            key_bindings=kb_elem,
         ),
     )
-    for i in range(10)
-]
+    w.bibcode = q.articles[i].bibcode
+    items.append(w)
+output.children = items
 
-boxes = [
-    Checkbox(
-        text=HTML(
-            f"{i:2d} {q.articles[i].title[0][:85]}\n"
-            f"   <u>{q.articles[i].first_author}</u> {q.articles[i].year} {q.articles[i].bibcode}"
-        )
-    )
-    for i in range(10)
-]
-
-checkbox1 = Checkbox(text="Checkbox")
-checkbox2 = Checkbox(text="Checkbox")
-
-
-root_container = HSplit(
-    [
-        # One window that holds the BufferControl with the default buffer on
-        # the left.
-        # Window(height=3, content=BufferControl(buffer=buffer1)),
-        # A vertical line in the middle. We explicitly specify the width, to
-        # make sure that the layout engine will not try to divide the whole
-        # width by three for all these windows. The window will simply fill its
-        # content by repeating this character.
-        # Window(char="-"),
-        # Display the text 'Hello world' on the right.
-        Frame(body=HSplit(items)),
-        # *items,
-        # checkbox1,
-        # checkbox2,
-        # VSplit([Frame(title="clsdkjf", body=HSplit(boxes))]),
-    ]
-)
-
+root_container = HSplit([searchbar, output, statusbar])
 layout = Layout(root_container)
+app = Application(layout=layout, full_screen=True, key_bindings=kb)
+app.page = 0
 
-app = Application(layout=layout, full_screen=False, key_bindings=kb)
-app.run()  # You won't be able to Exit this app
 
+def run():
+    result = app.run()
+    print(result)
+
+
+if __name__ == "__main__":
+    run()
